db5dc3120999b7a6747a50cce4844f737a882a55
jordy muniz <jordy.muniz96@gmail.com>
2021-06-02 08:52:58 -0500

    challange complete

---
+++ src/IfElse.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class IfElse {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        if(N % 2 != 0){
+            System.out.println("Weird");
+        }else if(N % 2 == 0 && 2 <= N && N < 5){
+            System.out.println("Not Weird");
+        }else if(N % 2 == 0 && 6 <= N && N < 20){
+            System.out.println("Weird");
+        }else if(N % 2 == 0 && N > 20){
+            System.out.println("Not Weird");
+        }
+
+    }
+}
