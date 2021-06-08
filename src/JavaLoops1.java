a5f6df8d7e56a0db951e7748393dd84577fc3ca1
jordy muniz <jordy.muniz96@gmail.com>
2021-06-08 11:29:33 -0500

    challange complete

---
+++ src/JavaLoops1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class JavaLoops1 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a integer");
+        int N = scan.nextInt();
+        int i = 1;
+        for(i = 1; i <= 10; i++){
+            System.out.println(N + " * " + i + " = " + N * i);
+        }
+    }
+}
