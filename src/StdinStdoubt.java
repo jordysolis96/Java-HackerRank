f4d6e2027e2ac0dabeee81be5b0c662e0ad435c8
jordy muniz <jordy.muniz96@gmail.com>
2021-06-16 10:24:46 -0500

    update

--- src/StdinStdoubt.java
+++ src/StdinStdoubt.java
@@ -4,11 +4,11 @@ public class StdinStdoubt {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter your first number");
+        System.out.println("enter your first number:");
         int a = scan.nextInt();
-        System.out.println("enter your second number");
+        System.out.println("enter your second number:");
         int b = scan.nextInt();
-        System.out.println("enter your third number");
+        System.out.println("enter your third number:");
         int c = scan.nextInt();
         System.out.println("Here are your numbers:");
         System.out.println("No.1: " + a);
