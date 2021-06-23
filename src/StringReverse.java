9368e86617aff728b57d4dcfb1b83a08fa58d21d
jordy muniz <jordy.muniz96@gmail.com>
2021-06-23 17:26:29 -0500

    update

--- src/StringReverse.java
+++ src/StringReverse.java
@@ -6,10 +6,10 @@ public class StringReverse {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String A = sc.nextLine();
+        String S = sc.nextLine();
         /* Enter your code here. Print output to STDOUT. */
-        String reverse = new StringBuffer(A).reverse().toString();
-        if(reverse.matches(A)){
+        String reverse = new StringBuffer(S).reverse().toString();
+        if(reverse.matches(S)){
             System.out.println("Yes");
         }else{
             System.out.println("No");
